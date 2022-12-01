543ac5dc0c90e5779fc8152dc8a2727ff67e3579
Sanchita Weljali <102817222+SanchitaWeljali@users.noreply.github.com>
2022-12-01 21:16:35 +0530

    Create 002_Find_Min_and_Max.cpp

---
+++ 002_Find_Min_and_Max.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+pair<long long, long long> getMinMax(long long a[], int n) ;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        ll a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        pair<ll, ll> pp = getMinMax(a, n);
+
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+      long long minE=a[0],maxE=a[0];
+      
+      for(int i=0;i<n;i++){
+          minE = min(a[i],minE);
+          maxE = max(a[i],maxE);
+      }
+      return {minE, maxE};
+    }
+    
